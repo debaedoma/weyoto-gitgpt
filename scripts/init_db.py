7e7d9c81f8238d21f1049f8f5ec688d73603788b
debaedoma <osadebaemma@gmail.com>
2025-04-18 19:26:27 +0100

    Initial commit

--- scripts/init_db.py
+++
@@ -1,9 +0,0 @@
-# This script creates the tables from the models
-from app import create_app
-from extensions import db
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("✅ All tables created in weyoto_gitgpt.")
